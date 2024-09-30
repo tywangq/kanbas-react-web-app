99118812094396431b687ff21d9172b54f66057c
ty <ty@tys-Macbook.local>
2024-09-29 23:02:19 -0700

    a2 1.0

---
+++ src/Labs/Lab2/Borders.tsx
@@ -0,0 +1,9 @@
+export default function Borders() {
+  return (
+    <div id="wd-css-borders">
+      <h2>Borders</h2>
+      <p className="border-fat border-red border-solid">Solid fat red border</p>
+      <p className="border-thin border-blue border-dashed">Dashed thin blue border </p>
+    </div>
+  );
+}
--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -1,7 +1,137 @@
+import "./index.css";
+import ForegroundColors from "./ForegroundColors";
+import BackgroundColors from "./BackgroundColors";
+import Borders from "./Borders";
+import Padding from "./Padding";
+import Margins from "./Margins";
+import Corners from "./Corners";
+import Dimensions from "./Dimensions";
+import { RelativePosition, AbsolutePosition, FixedPosition } from "./Positions";
+import Zindex from "./Zindex";
+import Float from "./Float";
+import GridLayout from "./GridLayout";
+import Flex from "./Flex";
+
 export default function Lab2() {
   return (
     <div id="wd-lab2">
-      <h2>Lab 2</h2>
+
+      <h2>Lab 2 - Cascading Style Sheets</h2>
+
+      {/* style attribute */}
+      {/* <h2>Styling with the STYLE attribute</h2> */}
+      {/* <p style={{
+        backgroundColor: "blue",
+        color: "white"
+      }}> */}
+
+      {/* external file */}
+      {/* <p>
+        Style attribute allows configuring look and feel
+        right on the element. Although it's very convenient
+        it is considered bad practice and you should avoid
+        using the style attribute.
+      </p> */}
+
+      {/* id selector */}
+      <div id="wd-css-id-selectors">
+        <h2>ID selectors</h2>
+        <p id="wd-id-selector-1">
+          Instead of changing the look and feel of all the
+          elements of the same name, e.g., P, we can refer to a specific element by its ID
+        </p>
+        <p id="wd-id-selector-2">
+          Here's another paragraph using a different ID and a different look and feel
+        </p>
+      </div>
+
+      {/* class selector */}
+      <div id="wd-css-class-selectors">
+        <h2>Class selectors</h2>
+        <p className="wd-class-selector">
+          Instead of using IDs to refer to elements, you can use an element's CLASS attribute
+        </p>
+        <h4 className="wd-class-selector">
+          This heading has same style as paragraph above
+        </h4>
+      </div>
+
+      {/* descendant & child selector */}
+      <div id="wd-css-document-structure">
+        <div className="wd-selector-1">
+          <h2>Document structure selectors</h2>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in particular
+            places in the document
+            <p className="wd-selector-3">
+              This paragraph's red background is referenced as
+              <br />
+              .selector-2 .selector3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                Whereas this span is a direct child of its parent
+              </span><br />
+              You can combine these relationships to create specific
+              styles depending on the document structure
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* color */}
+      <ForegroundColors />
+      <BackgroundColors />
+
+      {/* box model */}
+      <h2 style={{
+        backgroundColor: "yellow"
+      }}>
+        Block vs. inline elements
+      </h2>
+      <p style={{
+        backgroundColor: "blue",
+        color: "white"
+      }}>
+        Headings and paragraphs are
+        block elements. Width is parent's
+        width</p>
+      Normal text renders inline
+      <br /><br />
+      <span style={{
+        backgroundColor: "red", color: "white"
+      }}>Span elements</span>
+      <span style={{
+        backgroundColor: "red", color: "white"
+      }}>render inline</span>
+      with the rest of the content
+      <span style={{
+        backgroundColor: "red",
+        color: "white",
+        borderStyle: "solid",
+        borderWidth: "10px",
+        borderColor: "green"
+      }}>
+        Span elements</span>
+      <span style={{
+        backgroundColor: "red",
+        color: "white",
+        border: "solid 10px green"
+      }}>
+        render inline</span>
+
+      <Borders />
+      <Padding />
+      <Margins />
+      <Corners />
+      <Dimensions />
+      <RelativePosition />
+      <AbsolutePosition />
+      <FixedPosition />
+      <Zindex />
+      <Float />
+      <GridLayout />
+      <Flex />
+
     </div>
   );
 }
